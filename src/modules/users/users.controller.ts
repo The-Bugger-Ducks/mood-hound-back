5e08c46df6187e525c8bef621d386545c6f73b87
thhenrique <henriquethiago298@gmail.com>
2023-09-12 00:25:47 -0300

    feat: crud users

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,8 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	HttpCode,
+	HttpStatus,
+	Param,
+	ParseUUIDPipe,
+	Post,
+	Put,
+	Query,
+} from '@nestjs/common';
+
 import { ApiTags } from '@nestjs/swagger';
 
 import { UsersService } from './users.service';
+
 import { ActiveUserId } from 'src/shared/decorators/ActiveUserId.decorator';
+
+import { OptionalParseEnumPipe } from 'src/shared/pipes/OptionalParseEnumPipe';
+
+import { RoleEnum } from './entities/users.entity';
+
+import { CreateUserDto } from './dto/create.user.dto';
+import { UpdateUserDto } from './dto/update.user.dto';
+import { UpdateUserRoleDto } from './dto/update.userRole.dto';
 
 @Controller('users')
 @ApiTags('users')
@@ -12,5 +34,42 @@ export class UsersController {
 	@Get('/me')
 	me(@ActiveUserId() userId: string) {
 		return this.usersService.getUserById(userId);
+	}
+
+	@Get('/search')
+	search(
+		@ActiveUserId() userId: string,
+		@Query('name') name?: string,
+		@Query('email') email?: string,
+		@Query('role', new OptionalParseEnumPipe(RoleEnum)) role?: RoleEnum,
+	) {
+		return this.usersService.search(userId, {
+			name,
+			email,
+			role,
+		});
+	}
+
+	@Post()
+	create(@Body() createUserDto: CreateUserDto) {
+		return this.usersService.create(createUserDto);
+	}
+
+	@Put()
+	@HttpCode(HttpStatus.NO_CONTENT)
+	update(@ActiveUserId() userId: string, @Body() updateUserDto: UpdateUserDto) {
+		return this.usersService.update(userId, updateUserDto);
+	}
+
+	@Put('/role')
+	@HttpCode(HttpStatus.NO_CONTENT)
+	manageRole(@Body() updateUserRoleDto: UpdateUserRoleDto) {
+		return this.usersService.updateRole(updateUserRoleDto);
+	}
+
+	@Delete(':id')
+	@HttpCode(HttpStatus.NO_CONTENT)
+	delete(@Param('id', ParseUUIDPipe) userId: string) {
+		return this.usersService.remove(userId);
 	}
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,5 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+
+import { hash } from 'bcryptjs';
+
 import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
+
+import { RoleEnum } from './entities/users.entity';
+
+import { CreateUserDto } from './dto/create.user.dto';
+import { UpdateUserDto } from './dto/update.user.dto';
+import { UpdateUserRoleDto } from './dto/update.userRole.dto';
 
 @Injectable()
 export class UsersService {
@@ -8,7 +17,98 @@ export class UsersService {
 	getUserById(userId: string) {
 		return this.usersRepo.findUnique({
 			where: { id: userId },
-			select: { name: true, email: true },
+			select: { name: true, email: true, role: true },
 		});
+	}
+
+	search(
+		userId: string,
+		filters: {
+			name?: string;
+			email?: string;
+			role?: RoleEnum;
+		},
+	) {
+		return this.usersRepo.findMany({
+			where: {
+				id: { not: userId },
+				name: { contains: filters.name },
+				email: { contains: filters.email },
+			},
+			select: {
+				id: true,
+				email: true,
+				name: true,
+				role: true,
+			},
+		});
+	}
+
+	async create(createUserDto: CreateUserDto) {
+		const { name, role, email, password } = createUserDto;
+
+		const emailTaken = await this.usersRepo.findUnique({
+			where: { email },
+			select: { id: true },
+		});
+
+		if (emailTaken) {
+			throw new ConflictException('This email is already in use.');
+		}
+
+		const hashedPassword = await hash(password, 12);
+
+		return this.usersRepo.create({
+			data: {
+				name: name,
+				email: email,
+				password: hashedPassword,
+				role: role,
+			},
+			select: {
+				id: true,
+				name: true,
+				email: true,
+				role: true,
+			},
+		});
+	}
+
+	async update(userId: string, updateUserDto: UpdateUserDto) {
+		if (updateUserDto.password) {
+			const password = await hash(updateUserDto.password, 12);
+
+			updateUserDto.password = password;
+		}
+
+		await this.usersRepo.update({
+			where: { id: userId },
+			data: updateUserDto,
+		});
+
+		return null;
+	}
+
+	async updateRole(updateRoleUserDto: UpdateUserRoleDto) {
+		const { userId, role } = updateRoleUserDto;
+
+		await this.usersRepo.update({
+			where: { id: userId },
+			data: {
+				role: role,
+			},
+		});
+
+		return null;
+	}
+
+	async remove(userId: string) {
+		await this.usersRepo.delete({
+			where: {
+				id: userId,
+			},
+		});
+
+		return null;
 	}
 }
