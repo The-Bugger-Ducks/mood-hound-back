f6c13cd6f6a24fe0c378480fd95ff3b9ccc8a96c
Thiago Henrique Ferreira <henriquethiago298@gmail.com>
2023-09-25 00:07:52 -0300

    Development (#11)
    * Feat/#0901 manage users (#6)
    
    * feat: crud users
    
    * feat: crud users, routes in insomnia
    
    * fix: users crud, add filter by role
    
    * Feat/#0702 search comments (#9)
    
    * chore: set db default mongodb
    
    * feat: #0702 create paginator for requests
    
    * fix: remove ParseUUID in DTOS
    
    * feat: search comments and paginate result
    
    * fix: review change by joao of PR #16
    
    * Feat/seed db (#10)
    
    * chore: set db default mongodb
    
    * feat: #0702 create paginator for requests
    
    * fix: remove ParseUUID in DTOS
    
    * feat: search comments and paginate result
    
    * fix: review change by joao of PR #16
    
    * feat: create seed db
    
    * fix: add CostBenefit and Others in commentTopicEnum

--- src/shared/pipes/OptionalParseUUIDPipe.ts
+++
@@ -1,9 +0,0 @@
-import { ArgumentMetadata, ParseUUIDPipe } from '@nestjs/common';
-
-export class OptionalParseUUIDPipe extends ParseUUIDPipe {
-	override transform(value: string, metadata: ArgumentMetadata) {
-		if (typeof value === 'undefined') return undefined;
-
-		return super.transform(value, metadata);
-	}
-}
