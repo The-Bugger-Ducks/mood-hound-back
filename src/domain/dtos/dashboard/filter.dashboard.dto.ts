8658d2d7f356b2b81027fabc8301d1f737ef94e8
thhenrique <henriquethiago298@gmail.com>
2023-10-17 23:34:15 -0300

    hotfix: dashboard month filters

--- src/domain/dtos/dashboard/filter.dashboard.dto.ts
+++ src/domain/dtos/dashboard/filter.dashboard.dto.ts
@@ -1,19 +1,39 @@
-import { IsDateString, IsEnum, IsString } from 'class-validator';
+import { IsDate, IsEnum, IsOptional, IsString } from 'class-validator';
 import { PartialType } from '@nestjs/swagger';
 
 import { CommentTopicEnum } from '../../../domain/entities/comment.entity';
+import { Transform } from 'class-transformer';
 
 class FilterDashboard {
+	@IsOptional()
 	@IsEnum(CommentTopicEnum)
 	topic: CommentTopicEnum;
 
+	@IsOptional()
 	@IsString()
 	state: string;
 
-	@IsDateString()
+	@IsOptional()
+	@Transform(({ value }) => {
+		const date = new Date(value)
+		if (Date.prototype.toString.call(date) == 'Invalid Date') {
+			return value
+		}
+		return date
+	})
+	@IsDate()
 	dateStart: Date;
 
-	@IsDateString()
+	@IsOptional()
+	@Transform(({ value }) => {
+		const date = new Date(value)
+		if (Date.prototype.toString.call(date) == 'Invalid Date') {
+			return value
+		}
+		const lastDayOfMonth = new Date(date.getUTCFullYear(), date.getUTCMonth() + 1, 0)
+		return lastDayOfMonth
+	})
+	@IsDate()
 	dateEnd: Date;
 }
 
